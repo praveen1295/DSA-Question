37e0ab88cd6003f06e4f91c33c02ebe6d9e2de16
Praveen Bankhede <100555115+praveen1295@users.noreply.github.com>
2022-06-25 17:35:14 +0530

    Create Creation_and_traversal.java

---
+++ Linked_List/Creation_and_traversal.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+import javax.sound.sampled.SourceDataLine;
+
+class Single_LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head = null;
+
+    public void Creation(int data) {
+        Node new_node = new Node(data);
+        if (head == null) {
+            head = new_node;
+        } else {
+            new_node.next = head;
+            head = new_node;
+        }
+    }
+
+    public void traversal() {
+        Node temp = head;
+        if (head == null)
+            System.out.println("Linkedlist does not exist");
+        else {
+            while (temp != null) {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Single_LinkedList ll = new Single_LinkedList();
+        ll.Creation(1);
+        ll.Creation(2);
+        ll.Creation(3);
+        ll.Creation(4);
+        ll.Creation(5);
+
+        ll.traversal();
+    }
+}
